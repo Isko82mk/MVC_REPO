6a7fb551365898f80e753c9eb04707649b528650
Ivica <janevski.ivica@yahoo.com>
2020-09-01 16:21:30 +0200

    HomeWork2

---
+++ App2/HomeWork2/Controllers/AboutController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HomeWork2.Controllers
+{
+    [Route("Index/[action]")]
+    public class AboutController : Controller
+    {
+        public IActionResult AboutPage()
+        {
+            return View();
+        }
+
+        [Route("bridges")]
+        public ActionResult SecondPage()
+        {
+            return View();
+        }
+
+
+        public ActionResult Bridges()
+        {
+            return View("secondPage");
+        }
+
+
+        public ActionResult JsonGGBridge()
+        {
+            var bridge = new
+            {
+                Name = "Golden Gate Bridge,",
+                Width = "2,737 m",
+                Height = " 227 m ",
+                City = "San Francisco",
+            };
+
+            return new JsonResult(bridge);
+        }
+
+
+
+
+    }
+}
---
+++ App2/HomeWork2/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HomeWork2.Controllers
+{
+     
+    public class HomeController : Controller
+    {   
+         
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Redirect(int? id)
+        {
+            if (id.HasValue)
+            {
+                return View("order");
+            }
+            return View("index");
+        }
+
+        public IActionResult Redirect1(int?id)
+        {
+            if (id.HasValue)
+            {
+                return View("order");
+            }
+            return new RedirectResult("index");
+        }
+
+
+
+        public IActionResult Order()
+        {
+            return View();
+        }
+
+   
+
+
+
+
+
+    }
+}
